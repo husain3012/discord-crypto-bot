e6bb5e886b4ecb5edd9552f8126b67a436c4ccad
Husain <hsr3012kalimg@gmail.com>
2021-05-26 16:14:59 +0530

    add realtime stats in desc

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
 // jshint esversion:8
-
+// Add to your server: https://discord.com/oauth2/authorize?client_id=846618544560144394&scope=bot
 // permission integer:3959946304
 
 const Discord = require("discord.js");
 const axios = require("axios");
 const _ = require("lodash");
+const currSymbol = require(__dirname + "/currency-symbol.json");
 require("dotenv").config();
 
 const client = new Discord.Client();
-let base_curr="inr";
+let base_curr = "inr";
 var options = {
   method: "GET",
   url: "https://coingecko.p.rapidapi.com/coins/markets",
@@ -18,15 +19,38 @@ var options = {
     "x-rapidapi-host": "coingecko.p.rapidapi.com",
   },
 };
+let topCoins = ["btc", "eth", "doge"];
 
 client.on("ready", (err) => {
   if (!err) {
     console.log("Bot connected");
+    let i =0;
+    setInterval(function(){
+      console.log("making api request");
+      axios.request(options).then(function(response){
+        let array = response.data;
+        array.every((obj) => {
+          if (obj.symbol === topCoins[i]) {
+            client.user.setActivity(_.toUpper(obj.symbol) + " @ " + _.toUpper(base_curr) + formatNumber(obj.current_price) );
+            if(i<topCoins.length){
+              i +=1;
+            }
+            else(i = 0)
+            
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+      })
+      
+    }, 30000)
   }
 });
 
 client.on("message", (msg) => {
-  let command = msg.content.slice(0,5);
+  let command = msg.content.slice(0, 5);
   if (msg.content.slice(0, 5) === "stonk") {
     let coin = msg.content.slice(5);
     axios
@@ -35,8 +59,8 @@ client.on("message", (msg) => {
         let array = response.data;
         array.every((obj) => {
           if (obj.symbol === coin) {
-           let desc =  messageGenerator(obj);
-           msg.channel.send(sendEmbedMessage(obj, desc));
+            let desc = messageGenerator(obj);
+            msg.channel.send(sendEmbedMessage(obj, desc));
             return false;
           } else {
             return true;
@@ -49,17 +73,22 @@ client.on("message", (msg) => {
   }
 });
 
+
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
 function sendEmbedMessage(embedObj, message) {
   let Embed = new Discord.MessageEmbed()
-  .setTitle(embedObj.name)
-  .setColor(0x02475e)
-  .setDescription(message)
-  .setThumbnail(embedObj.image);
+    .setTitle(embedObj.name)
+    .setColor(0x02475e)
+    .setDescription(message)
+    .setThumbnail(embedObj.image);
   return Embed;
 }
 
 function messageGenerator(obj) {
-  return "High(24h): " + obj.high_24h + ",   Low(24h): " + obj.low_24h + ",   Current Price: " + obj.current_price;
+  return "High(24h): " + formatNumber(obj.high_24h) + ",   Low(24h): " + formatNumber(obj.low_24h) + ",   Current Price: " + formatNumber(obj.current_price);
 }
 
 client.login(process.env.BOTTOKEN);
