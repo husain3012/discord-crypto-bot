b8300c7d770970343987d02f016f5946e9e87305
Husain <hsr3012kalimg@gmail.com>
2021-05-26 15:01:51 +0530

    change APIs

--- bot.js
+++ bot.js
@@ -3,60 +3,63 @@
 // permission integer:3959946304
 
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const axios = require("axios");
 const _ = require("lodash");
 require("dotenv").config();
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Discord.Client();
+let base_curr="inr";
+var options = {
+  method: "GET",
+  url: "https://coingecko.p.rapidapi.com/coins/markets",
+  params: { vs_currency: base_curr, page: "1", per_page: "100", order: "market_cap_desc" },
+  headers: {
+    "x-rapidapi-key": process.env.COINGECKOKEY,
+    "x-rapidapi-host": "coingecko.p.rapidapi.com",
+  },
+};
+
+client.on("ready", (err) => {
+  if (!err) {
+    console.log("Bot connected");
+  }
 });
 
 client.on("message", (msg) => {
+  let command = msg.content.slice(0,5);
   if (msg.content.slice(0, 5) === "stonk") {
-    // Send "pong" to the same channel
-    let currency = msg.content.slice(5);
-    getCrypto(currency);
-  }
-
-  function getCrypto(currency) {
-    axios.get("https://api.wazirx.com/api/v2/tickers").then((resp) => {
-      const json = resp.data;
-      if (resp.status === 200) {
-        if (currency === "btc") {
-          let name = json.btcinr.name;
-          let high = json.btcinr.high;
-          let low = json.btcinr.low;
-          let volume = json.btcinr.volume;
-
-          let message = messageGenerator([name,high,low,volume]);
-          msg.channel.send(sendEmbedMessage(name, message));
-        } else if (currency === "eth") {
-          let name = json.ethinr.name;
-          let high = json.ethinr.high;
-          let low = json.ethinr.low;
-          let volume = json.ethinr.volume;
-          let message = messageGenerator([name,high,low,volume]);
-          msg.channel.send(sendEmbedMessage(name, message));
-        }
-      }
-    });
+    let coin = msg.content.slice(5);
+    axios
+      .request(options)
+      .then(function (response) {
+        let array = response.data;
+        array.every((obj) => {
+          if (obj.symbol === coin) {
+           let desc =  messageGenerator(obj);
+           msg.channel.send(sendEmbedMessage(obj, desc));
+            return false;
+          } else {
+            return true;
+          }
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   }
 });
 
-
-
-
-function sendEmbedMessage(title, message) {
+function sendEmbedMessage(embedObj, message) {
   let Embed = new Discord.MessageEmbed()
-    .setTitle(title)
-    .setColor(0x02475e)
-    .setDescription(message)
-    return Embed;
+  .setTitle(embedObj.name)
+  .setColor(0x02475e)
+  .setDescription(message)
+  .setThumbnail(embedObj.image);
+  return Embed;
 }
 
-function messageGenerator(values) {
-  return "High: " + values[1] + ", Low: "+values[2] + ", Volume: " + values[3];
+function messageGenerator(obj) {
+  return "High(24h): " + obj.high_24h + ",   Low(24h): " + obj.low_24h + ",   Current Price: " + obj.current_price;
 }
 
 client.login(process.env.BOTTOKEN);
